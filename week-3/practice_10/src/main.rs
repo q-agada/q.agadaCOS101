ff2c544571015d497520e75a808651f37946488d
q-agada <queen.agada@pau.edu.ng>
2026-09-14 01:01:39 +0100

    Update main.rs
    fix a mistake in  practice_10

--- week-3/practice_10/src/main.rs
+++ week-3/practice_10/src/main.rs
@@ -1,17 +1,21 @@
 fn main() {
-    let a: f32 =20.0;
-    let b: f32 =4.0;
+    //addition
+    let sum = 5550 + 7310;
+    println!(" the sum of 5550 and 7310 = {}", sum);
 
-let sum = a + b;
-let difference = a - b;
-let product = a * b;
-let quotient: f32 = a / b;
-let remainder = a % b;
+    //subtraction
+    let difference: f32 = 95.5 - 4.3;
+    println!("the difference of 95.5 and 4.3 = {}", difference);
 
-println!(" sum is {}", sum);
-println!(" difference is {}", difference);
-println!(" product is {}", product);
-println!(" quotient is {}", quotient);
-println!(" remainder is {}", remainder);
+    //product
+    let product: f32 = 4.0 * 30.0;
+    println!("the multiple of 4.0 and 30.0 = {}", product);
 
+    //quotient
+    let quotient: 56.7 / 32.2;
+    println!("the division of 56.7 and 32.2 = {}", quotient);
+
+    //remainder 
+    let remainder: 43 % 5;
+    println!("the remaindr of 43 and 5 = {}", remainder);
 }
